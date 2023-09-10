014cf6a79fd8ae0da338e77d1515bae61ec1233b
Juan27Pablo1 <143145695+Juan27Pablo1@users.noreply.github.com>
2023-09-10 05:03:01 +0000

    09/09/2023

---
+++ include/Mortal Kombat.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+class Personaje 
+{
+public:
+int Golpear();
+ int Patear();
+ int MoverIzquierda();
+ int MoverDerecha();
+ int Bloquear();
+ int Saltar();
+ int Caminar();
+};
+
+class Fatality{
+    +Golpear()
+    +Patear()
+
+}
+class Jugador {
+    +Seleccionar()
+    +MoverArriba()
+    +MoverAbajo()
+    +MoverIzquierda()
+    +MoverDerecha()
+}
+
+class Escenario
+
+class Combate{
+    +Seleccionar()
+}
+
+Class Puntuacion{
+    +Aumentar()
+}
+
+Class Tiempo{
+    +Diminuir()
+}
+Class Cinematica{
+    +Avanzar()
+}
+
+Class vsJugador
+
+Class vsCPU
+
+Class Salir{
+    +Cerrar()
+}
+
+Class Vida{
+    +Aumentar()
+    +Disminuir()
+}
+
+class Menu
+{
+    public:
+    +Seleccionar()
+    +MoverArriba()
+    +MoverAbajo()
+}
+
+class Opciones
+{
+public:
+    +Seleccionar()
+}
